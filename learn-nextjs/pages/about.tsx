c31abd112080ddcd8ed3656e01e13bcde37502e5
Ngoc Anh <anhln2@arrowhitech.net>
2022-06-20 09:31:47 +0700

    backup code

--- learn-nextjs/pages/about.tsx
+++ learn-nextjs/pages/about.tsx
@@ -1,5 +1,14 @@
+// import { Header } from '@/components/common/header';
+
 import { useRouter } from 'next/router'
 import  React from 'react'
+// import dynamic from 'next/dynamic';
+
+// const Header = dynamic(() => import('@/components/common/header') as any, {ssr: false});
+
+import dynamic from 'next/dynamic'
+
+ const Header = dynamic(() => import('@/components/common/header'), { ssr: false })
 
 export default function AboutPage () {
 
@@ -9,12 +18,13 @@ export default function AboutPage () {
   return (
     <div>
       <h1>About Page 1</h1>
+      <Header />
     </div>
   );
 }
 
-export async function getServerSideProps() {
-  return {
-    props: {}, // will be passed to the page component as props
-  }
-}
+// export async function getServerSideProps() {
+//   return {
+//     props: {}, // will be passed to the page component as props
+//   }
+// }
